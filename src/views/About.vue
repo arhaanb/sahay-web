e430e407855ee5cb4f057ce51d450a7719f2a39a
Arhaan Bahadur <arhaanb@gmail.com>
2021-06-27 07:45:10 +0530

    update vid

--- src/views/About.vue
+++ src/views/About.vue
@@ -92,7 +92,7 @@
       <div>
         <iframe
           width="100%"
-          src="https://www.youtube.com/embed/VfyERhwbi4c"
+          src="https://www.youtube.com/embed/D1K6Hmlyzs4"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
