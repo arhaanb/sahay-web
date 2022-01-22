47626398607407e51926ea78ea180341073562cf
Arhaan Bahadur <arhaanb@gmail.com>
2022-01-22 16:08:59 +0530

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -92,7 +92,7 @@
       <div>
         <iframe
           width="100%"
-          src="https://www.youtube.com/embed/_KCyHwJKC6M"
+          src="https://www.youtube.com/embed/llal9MEkpi0"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
